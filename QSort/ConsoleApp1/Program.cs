d6e6a24e1eaa5ca9a4745dcf59a7fa8f45bd9e97
vladcto <razrab.ytka@gmail.com>
2022-05-10 22:37:05 +0300

    Вынес логику сортировки в отдельный класс, сделал метод сортировки обобщенным. Сделал возможность выбирать как сравнивать обьекты при сортировке. Рефакторинг.

--- QSort/ConsoleApp1/Program.cs
+++ QSort/ConsoleApp1/Program.cs
@@ -5,30 +5,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Класс для работы с пользователем и форматирование входных/выходных данных.
 class Program
 {
     static void Main(string[] args)
     {
-        List<int> a = new List<int> { 1, 2, 3 };
-        ArraySegment<int> b = new ArraySegment<int>(a.ToArray(), 1, 1);
-        IList<int> words1 = b;
-        words1[0] = 3;
-        string inpPath = @"D:\Projects\GUAP.OP.1\QSort\ConsoleApp1\test1.txt";
+        string inpPath = @"D:\Projects\GUAP.OP.1\QSort\ConsoleApp1\test3.txt";
         using (StreamReader inpFile = new StreamReader(inpPath))
         {
             char[] textArr = inpFile.ReadToEnd().ToCharArray();
             IList<string> words = SelectWords(textArr);
-            QuickSort(words);
+            words.QuickSort();
             Console.WriteLine("res");
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 35; i++)
             {
-                Console.WriteLine(words[i]);
+                Console.WriteLine($"{i}:{words[i]}");
             }
         }
         Console.ReadKey();
     }
 
-    static private List<string> SelectWords(char[] buffer)
+    static private List<string> SelectWords(IList<char> buffer)
     {
         List<string> res = new List<string>();
         StringBuilder stringBuilder = new StringBuilder();
@@ -38,7 +35,7 @@ class Program
             {
                 stringBuilder.Append(symb);
             }
-            else
+            else                    
             {
                 if (stringBuilder.Length != 0)
                 {
@@ -49,40 +46,48 @@ class Program
         }
         return res;
     }
+}
 
-    static private void QuickSort(IList<string> arraySegment)
+//Класс для реализации алгоритмов (сортировки в данном случае)
+static public class Algorithm
+{
+    static public void QuickSort(this IList<string> arraySegment)
     {
-        QuickSort(arraySegment, 0, arraySegment.Count - 1);
+        Func<string, string, bool> func = (f, s) => string.Compare(f, s) > 0;
+        QuickSort<string>(arraySegment, 0, arraySegment.Count - 1, func);
     }
 
-    static private void QuickSort(IList<string> array,int first,int last)
+    static public void QuickSort<T>(this IList<T> arraySegment, Func<T, T, bool> comparator)
     {
-        string word = array[first];
+        QuickSort(arraySegment, 0, arraySegment.Count - 1,comparator);
+    }
+
+    static private void QuickSort<T>(IList<T> array, int first, int last,Func<T, T, bool> comparator)
+    {
+        T word = array[first];
         int f = first, l = last;
         do
         {
-            while (String.Compare(word, array[f]) > 0) f++;
-            while (String.Compare(word, array[l]) < 0) l--;
+            while (comparator(word, array[f])) f++;
+            while (comparator(array[l], word)) l--;
 
             if (f <= l)
             {
-                string tmp = array[f];
-                Console.WriteLine(f);
+                T tmp = array[f];
                 array[f] = array[l];
                 array[l] = tmp;
                 f++;
                 l--;
             }
-
         } while (f < l);
 
         if (l > first)
         {
-            QuickSort(array, first, l);
+            QuickSort(array, first, l,comparator);
         }
         if (f < last)
         {
-            QuickSort(array, f, last);
+            QuickSort(array, f, last,comparator);
         }
     }
 }
