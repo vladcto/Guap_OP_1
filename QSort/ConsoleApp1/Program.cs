2f543c9abc1168adf714787bf04c886a68b9c5f9
vladcto <razrab.ytka@gmail.com>
2022-06-01 22:35:43 +0300

    Сделал вывод результата в файлы. Рефакторинг.

--- QSort/ConsoleApp1/Program.cs
+++ QSort/ConsoleApp1/Program.cs
@@ -1,28 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 //Класс для работы с пользователем и форматирование входных/выходных данных.
 class Program
 {
     static void Main(string[] args)
     {
-        string inpPath = @"D:\Projects\GUAP.OP.1\QSort\ConsoleApp1\test3.txt";
+        Console.WriteLine("Введите путь до исходного файл:");
+        string inpPath = Console.ReadLine();
+        IList<string> words;
         using (StreamReader inpFile = new StreamReader(inpPath))
         {
             char[] textArr = inpFile.ReadToEnd().ToCharArray();
-            IList<string> words = SelectWords(textArr);
-            words.QuickSort();
-            Console.WriteLine("res");
-            for (int i = 0; i < 35; i++)
+            words = SelectWords(textArr);
+        }
+
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+        words.QuickSort();
+        stopWatch.Stop();
+        double seconds = stopWatch.Elapsed.TotalSeconds;
+
+        Console.WriteLine("Введите путь до файла-результата:");
+        WriteResult(Console.ReadLine(), words);
+        Console.WriteLine("Введите путь до файла-аналитики:");
+        WriteAnalysis(Console.ReadLine(), words, seconds);
+    }
+
+    static public void WriteAnalysis(string filePath, in IList<string> input, double secs)
+    {
+        using (StreamWriter outFile = new StreamWriter(filePath))
+        {
+            outFile.WriteLine($"Затрачено секунд = {secs}");
+            long symbols = input.Aggregate<string, long>(0, (aggr, word) => aggr += word.Length);
+            outFile.WriteLine($"Количество символов = {symbols}");
+            outFile.WriteLine($"Количество слов = {input.Count}");
+
+            var groupedResult = input.GroupBy(word => char.ToLower(word[0]))
+                .Select(group => $"{group.Key} = {group.Count()}");
+            foreach (string formatedRes in groupedResult)
             {
-                Console.WriteLine($"{i}:{words[i]}");
+                outFile.WriteLine(formatedRes);
+            }
+            outFile.Write("Количество слов на остальные буквы алфавита = 0");
+        }
+    }
+
+    static public void WriteResult(string filePath, in IList<string> input)
+    {
+        using (StreamWriter outFile = new StreamWriter(filePath))
+        {
+            var groupedResult = input.GroupBy(word => char.ToLower(word[0]))
+                .Select(group => string.Join(" ", group));
+            foreach (string formatedRes in groupedResult)
+            {
+                outFile.WriteLine(formatedRes);
             }
         }
-        Console.ReadKey();
     }
 
     static private List<string> SelectWords(IList<char> buffer)
@@ -31,18 +69,23 @@ class Program
         StringBuilder stringBuilder = new StringBuilder();
         foreach (char symb in buffer)
         {
-            if (char.IsLetterOrDigit(symb))
+            if (symb == ',' && int.TryParse(stringBuilder.ToString(), out _))
             {
                 stringBuilder.Append(symb);
             }
-            else                    
+            else if (char.IsLetterOrDigit(symb))
             {
-                if (stringBuilder.Length != 0)
-                {
-                    res.Add(stringBuilder.ToString());
-                    stringBuilder.Clear();
-                }
+                stringBuilder.Append(symb);
             }
+            else if (stringBuilder.Length != 0)
+            {
+                res.Add(stringBuilder.ToString());
+                stringBuilder.Clear();
+            }
+        }
+        if (stringBuilder.Length != 0)
+        {
+            res.Add(stringBuilder.ToString());
         }
         return res;
     }
@@ -51,25 +94,25 @@ class Program
 //Класс для реализации алгоритмов (сортировки в данном случае)
 static public class Algorithm
 {
-    static public void QuickSort(this IList<string> arraySegment)
+    static public void QuickSort(this IList<string> list)
     {
         Func<string, string, bool> func = (f, s) => string.Compare(f, s) > 0;
-        QuickSort<string>(arraySegment, 0, arraySegment.Count - 1, func);
+        QuickSort(list, 0, list.Count - 1, func);
     }
 
-    static public void QuickSort<T>(this IList<T> arraySegment, Func<T, T, bool> comparator)
+    static public void QuickSort<T>(this IList<T> list, Func<T, T, bool> comparator)
     {
-        QuickSort(arraySegment, 0, arraySegment.Count - 1,comparator);
+        QuickSort(list, 0, list.Count - 1, comparator);
     }
 
-    static private void QuickSort<T>(IList<T> array, int first, int last,Func<T, T, bool> comparator)
+    static private void QuickSort<T>(IList<T> array, int first, int last, Func<T, T, bool> comparator)
     {
-        T word = array[first];
+        T key = array[first];
         int f = first, l = last;
         do
         {
-            while (comparator(word, array[f])) f++;
-            while (comparator(array[l], word)) l--;
+            while (comparator(key, array[f])) f++;
+            while (comparator(array[l], key)) l--;
 
             if (f <= l)
             {
@@ -83,11 +126,11 @@ static public class Algorithm
 
         if (l > first)
         {
-            QuickSort(array, first, l,comparator);
+            QuickSort(array, first, l, comparator);
         }
         if (f < last)
         {
-            QuickSort(array, f, last,comparator);
+            QuickSort(array, f, last, comparator);
         }
     }
 }
